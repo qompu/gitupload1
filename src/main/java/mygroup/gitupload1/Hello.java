64b718fffbfb3c8eff7fdd15bb941cc41bfc5990
qompu <mtamayo11@hawkmail.hccfl.edu>
2021-02-14 20:59:28 -0500

    git upload 1

---
+++ src/main/java/mygroup/gitupload1/Hello.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mygroup.gitupload1;
+
+import java.util.Date;
+
+
+public class Hello {
+    
+    public static void main(String[] args) {
+		
+	System.out.println("HELLO NETBEANS 12.1 WORLD! The current time is " + new Date());  
+
+	}
+    
+}
